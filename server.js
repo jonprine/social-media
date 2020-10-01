56973e81fd3319a19e9bd48817a3e0f7b6e53b89
Vox Humana <jonprine@gmail.com>
2020-10-01 16:48:32 -0500

    added mongoose connect

--- server.js
+++ server.js
@@ -8,4 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
