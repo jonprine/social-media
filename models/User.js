8fae2fcfb3c44ab5cb82667a234e467f6e04da19
Vox Humana <jonprine@gmail.com>
2020-10-01 16:24:27 -0500

    initial user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true,
+     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Must use a valid email address']
+    }
+
+})
