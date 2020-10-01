33095fbe1fca21006020740a52721d7b65c0cbcb
Vox Humana <jonprine@gmail.com>
2020-10-01 16:46:26 -0500

    added express midware  and connected to server

--- models/User.js
+++ models/User.js
@@ -15,3 +15,9 @@ const UserSchema = new Schema({
     }
 
 })
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
