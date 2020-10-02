2d7ef73a5432037af715b78227e58cc601c3b14c
Vox Humana <jonprine@gmail.com>
2020-10-02 09:34:12 -0500

    added routes to incorporate users in thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -18,4 +18,10 @@ router
   .get(getThoughtById)
   .put(updateThought)
 
+  // /api/thoughts/<userId>
+router.route('/:userId').post(addThought);
+
+// /api/thoughts/<userId>/<thoughtId>
+router.route('/:userId/:thoughtId').delete(removeThought);
+
 module.exports = router;
