6ca5f3989da9eeddae18c580d865fa783733d1b3
Vox Humana <jonprine@gmail.com>
2020-10-02 09:36:48 -0500

    added prefix to routes for thoughts in index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
+// add prefix of `/thoughts` to routes created in `thought-routes.js`
+router.use('/thoughts', thoughtRoutes);
 // add prefix of `/users` to routes created in `pizza-routes.js`
 router.use('/users', userRoutes);
 
