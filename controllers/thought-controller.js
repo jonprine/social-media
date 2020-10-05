753e0d39a4d5d83fecf344f96f79d469562d442c
Vox Humana <jonprine@gmail.com>
2020-10-04 20:12:58 -0500

    fixed delete reaction in thoughtcontroller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -101,7 +101,7 @@ const thoughtController = {
   removeReaction({ params }, res) {
     Thought.findOneAndUpdate(
       { _id: params.thoughtId },
-      { $pull: { replies: { reactionId: params.reactionId } } },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
       { new: true }
     )
       .then((dbUserData) => res.json(dbUserData))
